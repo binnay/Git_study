b9d5bda05510585b30fab47b675168757706e62e
binna <imbinnay@gmail.com>
2024-11-29 06:50:47 +0000

    commit F

--- HelloGit.java
+++ HelloGit.java
@@ -2,7 +2,7 @@
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello5");
-        System.out.println("Hello");
+        System.out.println("Hello6");
 
     }
 }
